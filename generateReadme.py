ddb5f622379354464385f5e9fec5286a07b8aabd
szzdmj <chuanfuyin001@gmail.com>
2024-07-24 08:48:36 +0800

    Update generateReadme.py

--- generateReadme.py
+++ generateReadme.py
@@ -1,20 +1,20 @@
-# -*- coding: utf-8 -*-
 import urllib3
 from lxml import etree
 import html
 import re
 
-blogUrl = 'https://szmj0.github.io'
+# blogUrl = 'szmj0.github.io'
+blogUrl = 'http://www.shenzhouzhengdao.org/'
 
 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'} 
 
 def addIntro(f):
 	txt = '''  
 <p align="center">
-  <img src="https://github.com/szmj0/update/blob/main/extras/Icon-256.jpg"/>
+  <img src="github.com/szmj0/update/blob/main/extras/Icon-256.jpg"/>
 </p>	
 <p align="center">
-  <img src="https://github.com/szmj0/update/blob/main/extras/sjmj-fg.jpg"/>
+  <img src="github.com/szmj0/update/blob/main/extras/sjmj-fg.jpg"/>
 </p>
 
 <p align="center">test</p>  
@@ -26,9 +26,9 @@ def addIntro(f):
 def addProjectInfo(f):
 	txt ='''
 ### 开源项目  
-- [app](https://github.com/szmj0/update/blob/main/extras/szmj-v6.9.2024010901.apk)神州明见6.9	
+- [app](github.com/szmj0/update/blob/main/extras/szmj-v6.9.2024010901.apk)神州明见6.9	
    
-[查看更多](https://github.com/szmj0/Publish)	 
+[查看更多](github.com/szmj0/Publish)	 
 
 	''' 
 	f.write(txt) 
@@ -36,14 +36,26 @@ def addProjectInfo(f):
 def addZhuanlanInfo(f):
 	txt ='''
 ### 专栏  
-- [SZMJ WEB](https://github.com/szmj0/update/blob/main/extras/SZZD_PC/szmjweb.3.0.zip)
-- [list2.txt](https://szzdmj.github.io/github-page-test/list2.txt)
-- [dtw](https://j.mp/ddw2288)
+- [SZMJ WEB](github.com/szmj0/update/blob/main/extras/SZZD_PC/szmjweb.3.0.zip)
+- [list2.txt](szzdmj.github.io/github-page-test/list2.txt)
+- [dtw](j.mp/ddw2288)
 - ……
 
 	''' 
-	f.write(txt) 
 
+# add list2.txt
+	list2_filename = 'list2.txt'	
+	with open(list2_filename, "r+") as l:
+		data = l.read()
+	list2_text = '''
+**list2.txt:**"      
+---
+{data}
+---
+	'''.format(data=data)	
+
+	f.write(txt) 
+	f.write(list2_text)
 
 def addBlogInfo(f):  
 	http = urllib3.PoolManager(num_pools=5, headers = headers)
@@ -51,8 +63,6 @@ def addBlogInfo(f):
 	resp_tree = etree.HTML(resp.data.decode("utf-8"))
 	# html_data = resp_tree.xpath(".//div[@class='article-item-box csdn-tracking-statistics']/h4") 
 	html_data = resp_tree.xpath(".//article[@class='blog-list-box']")
-
- 
 
 if __name__=='__main__':
 	f = open('README.md', 'w+')
@@ -62,11 +72,10 @@ if __name__=='__main__':
 	addProjectInfo(f)
 	f.write('\n</td>\n')
 	f.write('<td valign="top" width="33%">\n')
-	addBlogInfo(f)
+	# addBlogInfo(f)
 	f.write('\n</td>\n')
 	f.write('<td valign="top" width="33%">\n')
 	addZhuanlanInfo(f)
 	f.write('\n</td>\n')
 	f.write('</tr></table>\n')
 	f.close 
-
