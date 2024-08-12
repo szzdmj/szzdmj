4e295a5f1ad99e7736867eb5d9ede44e510aaeb2
szzdmj <chuanfuyin001@gmail.com>
2024-08-12 23:16:59 +0800

    Update generateReadme.py

--- generateReadme.py
+++ generateReadme.py
@@ -8,7 +8,7 @@ import html2text
 
 
 # blogUrl = 'szmj0.github.io www.shenzhouzhengdao.org'
-blogUrl = 'szzdmj.github.io/szzdmj'
+blogUrl = 'szzdmj.github.io'
 
 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'} 
 
