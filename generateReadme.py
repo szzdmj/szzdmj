1dfb1692c1f7425809b86bc6a95aeea27addbad8
szzdmj <chuanfuyin001@gmail.com>
2024-08-12 23:47:23 +0800

    Update generateReadme.py

--- generateReadme.py
+++ generateReadme.py
@@ -68,7 +68,7 @@ def addHTMLInfo(f):
 
 	''' 
 	f.write(txt)
-	with open('index.html', 'r+', encoding='gbk') as file:
+	with open('index.html', 'r+', encoding='utf-8') as file:
     		html_content = file.read()
 	soup = BeautifulSoup(html_content, 'html.parser')  # or 'lxml'
 	articles = soup.find_all('pre')
